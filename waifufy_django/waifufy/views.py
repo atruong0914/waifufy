c86444682a1677e10ea631725f4075fc02f0bbcf
annika truong <annikatruongt@gmail.com>
2022-02-08 12:32:13 -0800

    added permissions to views must be auth

--- waifufy_django/waifufy/views.py
+++ waifufy_django/waifufy/views.py
@@ -2,40 +2,49 @@ from .serializers import UserSerializer, PlaylistSerializer, ArtistSerializer, S
 from .models import Playlist, Artist, Song
 from rest_framework import generics
 from accounts.models import User
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# Create your views here
 class UserList(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
 class PlaylistList(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Playlist.objects.all()
     serializer_class = PlaylistSerializer
 
 class PlaylistDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Playlist.objects.all()
     serializer_class = PlaylistSerializer
 
 
 class ArtistList(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Artist.objects.all()
     serializer_class = ArtistSerializer
 
 class ArtistDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Artist.objects.all()
     serializer_class = ArtistSerializer
 
 
 class SongList(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Song.objects.all()
     serializer_class = SongSerializer
 
 class SongDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Song.objects.all()
     serializer_class = SongSerializer
