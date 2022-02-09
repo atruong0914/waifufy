113c6544e2d2f85aa811279df0e93429f138eef7
annika truong <annikatruongt@gmail.com>
2022-02-09 13:17:34 -0800

    edited backend

--- waifufy_django/waifufy/urls.py
+++ waifufy_django/waifufy/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('artists/', views.ArtistList.as_view(), name='artist_list'),
     path('artists/<int:pk>', views.ArtistDetail.as_view(), name='artist_detail'),
     path('songs/', views.SongList.as_view(), name='song_list'),
-    path('songs/<int:pk>', views.SongDetail.as_view(), name='song_detail')
+    path('songs/<int:pk>', views.SongDetail.as_view(), name='song_detail'),
 ]
--- waifufy_django/waifufy/views.py
+++ waifufy_django/waifufy/views.py
@@ -1,6 +1,6 @@
 from .serializers import UserSerializer, PlaylistSerializer, ArtistSerializer, SongSerializer
 from .models import Playlist, Artist, Song
-from rest_framework import generics
+from rest_framework import generics, permissions, filters
 from accounts.models import User
 from rest_framework.permissions import IsAuthenticated
 
@@ -12,42 +12,50 @@ class UserList(generics.ListCreateAPIView):
 
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
 class PlaylistList(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
     queryset = Playlist.objects.all()
     serializer_class = PlaylistSerializer
 
 
 class PlaylistDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
     queryset = Playlist.objects.all()
     serializer_class = PlaylistSerializer
 
 
 class ArtistList(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
     queryset = Artist.objects.all()
     serializer_class = ArtistSerializer
 
 
 class ArtistDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
     queryset = Artist.objects.all()
     serializer_class = ArtistSerializer
 
 
 class SongList(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
     queryset = Song.objects.all()
     serializer_class = SongSerializer
 
 
 class SongDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
     queryset = Song.objects.all()
     serializer_class = SongSerializer
+
+
+#  song search
+class SongListFilter(generics.ListAPIView):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
+    filter_songs = [filters.SearchFilter]
+    search_fields = ['^slug']
