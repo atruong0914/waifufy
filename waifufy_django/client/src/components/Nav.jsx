e8c42d482905987ce0282aa9bed0a9608734b784
annika truong <annikatruongt@gmail.com>
2022-02-11 08:04:04 -0800

    artists.js set up

--- waifufy_django/client/src/components/Nav.jsx
+++ waifufy_django/client/src/components/Nav.jsx
@@ -14,10 +14,10 @@ import { Link } from 'react-router-dom'
     return (
       <div>
         <Link to='/'> Home </Link>
-        <Link to='/playlists' > Playlists </Link>
-        <Link to='/artists' > Artists </Link>
-        <Link to='/songs' > Songs </Link>
-        <Link to='/about' > About </Link>
+        <Link to='/playlists'> Playlists </Link>
+        <Link to='/artists'> Artists </Link>
+        <Link to='/songs'> Songs </Link>
+        <Link to='/about'> About </Link>
         <Link to='/' onClick={logOut}> Log out </Link>
 
       </div>
--- waifufy_django/client/src/pages/Artists.js
+++ waifufy_django/client/src/pages/Artists.js
@@ -1,7 +1,41 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useParams, useNavigate, Link } from 'react-router-dom'
+import axios from 'axios'
+import { BASE_URL } from '../globals'
 
 export default function Artists() {
+  const { id } = useParams()
+  const navigate = useNavigate()
+  const [loading, setLoading] = useState(true)
+
+  const [artists, setArtists] = useState([])
+
+  useEffect(() => {
+    async function getArtists() {
+      let artists = await axios.get(`${BASE_URL}artists/`)
+      setArtists(artists.data.artists)
+    }
+  }, [])
+  
   return (
-    <div>Artists</div>
+    <div className='artist-display'>
+      <div className='artist-header'>
+        header
+      </div>
+      <div className='artists'>
+        {artists.map((artist, index) => (
+          <Artist
+          key={index}
+          id={artist.id}
+          name={artist.name}
+          bio={artist.bio}
+          artist_image={artist.artist_image}
+          songs={artist.songs}
+          />
+        ))}
+      </div>
+    
+    
+    </div>
   )
 }
