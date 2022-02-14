1c188a36b986fcf082c3b57a25919cf8ada3fb69
atruong0914 <annikatruongt@gmail.com>
2022-02-14 07:31:03 -0600

    styling

--- waifufy_django/client/src/components/Footer.jsx
+++ waifufy_django/client/src/components/Footer.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import '../App.css'
+
+const gitHub = () => {
+  window.location.href='https://github.com/atruong0914/waifufy'
+}
+
+const linkedIn = () => {
+  window.location.href='https://www.linkedin.com/in/annikatruong/'
+}
+
+const instaGram = () => {
+  window.location.href='https://www.instagram.com/ign.xaster/'
+}
+
+const twitchTV = () => {
+  window.location.href='https://www.twitch.tv/xasterttv'
+}
+
+
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <GitHubIcon onClick={() => {gitHub()}} className='icon-footer'/>
+      <LinkedInIcon onClick={() => {linkedIn()}} className='icon-footer'/>
+      <InstagramIcon onClick={() => {instaGram()}} className='icon-footer'/>
+      <LiveTvIcon onClick={() => {twitchTV()}} className='icon-footer'/>
+    </div>
   )
 }
